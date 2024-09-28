896e7a8b29e0da788c626ce959acb62d9ea41b7a
escape-engineering <tkdrnr1215@naver.com>
2024-09-28 23:04:23 +0900

    feat: get itemLists

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -1,7 +1,20 @@
+import { getItemsList } from "@/utils/serverApi";
 import React from "react";
 
-const ItemsPage = () => {
-    return <div>ItemsPage</div>;
+const ItemsPage = async () => {
+    const items = await getItemsList();
+    console.log("items :>> ", items);
+    return (
+        <div>
+            {items.map((item) => {
+                return (
+                    <div key={`${item.name}+${item.plaintext}`}>
+                        <h3>{item.name}</h3>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default ItemsPage;
--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,4 +1,5 @@
 import { ChampionDetail, ChampionList } from "@/types/Champion";
+import { ItemDetail } from "@/types/Item";
 
 const getCurrentVersion = async () => {
     const versionRes = await fetch(`${process.env.NEXT_PUBLIC_DDRAGON_URL}/api/versions.json`);
@@ -27,4 +28,22 @@ const getChampionDetail = async (championName: string) => {
     return championValues;
 };
 
-export { getChampionList, getCurrentVersion, getChampionDetail };
+const getItemsList = async () => {
+    const currentVersion = await getCurrentVersion();
+    const itemRes = await fetch(`${process.env.NEXT_PUBLIC_DDRAGON_URL}/cdn/${currentVersion}/data/ko_KR/item.json`);
+    const itemData: ItemDetail = await itemRes.json();
+    const itemList = Object.values(itemData.data).map((item) => {
+        return {
+            name: item.name,
+            description: item.description,
+            plaintext: item.plaintext,
+            into: item.into,
+            image: item.image,
+            gold: item.gold,
+            tags: item.tags,
+        };
+    });
+    return itemList;
+};
+
+export { getChampionList, getCurrentVersion, getChampionDetail, getItemsList };
