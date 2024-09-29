07e261778b2788c5a9438d6d81f2eba01655893d
escape-engineering <tkdrnr1215@naver.com>
2024-09-29 19:38:56 +0900

    feat: detailpage 데이터 화면에 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,32 @@ const nextConfig = {
     reactStrictMode: false,
     images: {
         // https://ddragon.leagueoflegends.com/cdn/14.19.1/img/champion/${cham.id}.png
+        // https://ddragon.leagueoflegends.com/cdn/14.19.1/img/spell/${spell.id}.png
         remotePatterns: [
             {
                 protocol: "https",
                 hostname: "ddragon.leagueoflegends.com",
                 port: "",
-                pathname: "/cdn/14.19.1/img/champion/**",
+                pathname: "**",
             },
+            // {
+            //     protocol: "https",
+            //     hostname: "ddragon.leagueoflegends.com",
+            //     port: "",
+            //     pathname: "/cdn/14.19.1/img/champion/**",
+            // },
+            // {
+            //     protocol: "https",
+            //     hostname: "ddragon.leagueoflegends.com",
+            //     port: "",
+            //     pathname: "/cdn/img/champion/splash/**_**.jpg",
+            // },
+            // {
+            //     protocol: "https",
+            //     hostname: "ddragon.leagueoflegends.com",
+            //     port: "",
+            //     pathname: "/cdn/14.19.1/img/spell/**.png",
+            // },
         ],
     },
 };
